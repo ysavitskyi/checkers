138b398be2883cf659286d0269b00c357c61a101
Yevhen Savitskyi <yevhen.savitskyi@gmail.com>
2023-04-27 12:11:59 +0300

    upd very dummy behavior

--- src/App.tsx
+++ src/App.tsx
@@ -33,11 +33,12 @@ const Piece: React.FC<{
 const Cell: React.FC<{
   id: string
   isEmpty: boolean
-  player1: boolean
-  player2: boolean
+  occupied: 'player1' | 'player2' | null
+  disabled: boolean
   isActive: boolean
+  highlighted: boolean
   onClick: (id: string, isPieceTarget?: boolean) => void
-}> = ({ id, isEmpty, player1, player2, isActive, onClick }) => {
+}> = ({ id, isEmpty, occupied, isActive, disabled, highlighted, onClick }) => {
   const onCellClick = useCallback(
     (event: React.MouseEvent<HTMLDivElement>) => {
       onClick(id)
@@ -57,11 +58,16 @@ const Cell: React.FC<{
     <div className="cell cell--empty" />
   ) : (
     <div
-      className={composeCls(['cell'], { 'cell--active': isActive })}
+      className={composeCls(['cell'], {
+        'cell--active': isActive,
+        'cell--disabled': disabled,
+        'cell--highlighted': highlighted,
+      })}
       onClick={onCellClick}
     >
-      {player1 && <Piece onClick={onPieceClick} isWhite />}
-      {player2 && <Piece onClick={onPieceClick} />}
+      {occupied && (
+        <Piece onClick={onPieceClick} isWhite={occupied === 'player1'} />
+      )}
     </div>
   )
 }
@@ -70,45 +76,97 @@ const grid = [...Array(GRID_SIZE ** 2)].map((_, i) => {
   const isEmpty = (((i / GRID_SIZE) ^ 0) + i + 1) % 2 > 0
   const player1 = i < GRID_SIZE * (GRID_SIZE / 2 - 1)
   const player2 = i > GRID_SIZE * (GRID_SIZE / 2 + 1) - 1
+  const occupied: TCellOccupied =
+    (player1 && 'player1') || (player2 && 'player2') || null
 
   return {
     id: String(i),
-    player1,
-    player2,
+    occupied,
     isEmpty,
   }
 })
 
+const calcAvailableCells = (
+  cellsById: Record<string, ICellState>,
+  id: string
+) => {
+  const currentCell = cellsById[id]
+  const availableCells: string[] = []
+
+  const { oppositePlayer, incPos1, incPos2 } = (() => {
+    if (currentCell.occupied === 'player1') {
+      return {
+        oppositePlayer: 'player2',
+        incPos1: GRID_SIZE - 1,
+        incPos2: GRID_SIZE + 1,
+      }
+    } else {
+      return {
+        oppositePlayer: 'player1',
+        incPos1: -(GRID_SIZE - 1),
+        incPos2: -(GRID_SIZE + 1),
+      }
+    }
+  })()
+
+  const checkNextPos = (
+    currentCell: ICellState,
+    increment: number,
+    iteration = 0
+  ) => {
+    const nextPos = +currentCell.id + increment
+
+    if (!cellsById[nextPos]) return
+
+    if (cellsById[nextPos].occupied === null) {
+      availableCells.push(`${nextPos}`)
+    } else if (cellsById[nextPos].occupied === oppositePlayer) {
+      checkNextPos(cellsById[nextPos], increment, iteration++)
+    }
+  }
+
+  checkNextPos(currentCell, incPos1)
+  checkNextPos(currentCell, incPos2)
+
+  return availableCells
+}
+
+type TCellOccupied = 'player1' | 'player2' | null
 interface ICellState {
   id: string
-  player1: boolean
-  player2: boolean
+  occupied: TCellOccupied
 }
 const Grid: React.FC = () => {
   const [state, setState] = useState<{
     cellsById: Record<string, ICellState>
     activePieceId: string | null
+    availableCells: string[]
+    player1Turn: boolean
   }>({
     cellsById: grid.reduce(
-      (acc, { id, isEmpty, player1, player2 }) => ({
+      (acc, { id, isEmpty, occupied }) => ({
         ...acc,
         ...(!isEmpty && {
           [id]: {
             id,
-            player1,
-            player2,
+            occupied,
           },
         }),
       }),
       {}
     ),
+
     activePieceId: null,
+    availableCells: [],
+    player1Turn: true,
   })
 
   const onCellClick = useCallback((id: string, isPieceTarget?: boolean) => {
     setState((state) => {
       if (isPieceTarget) {
-        return { ...state, activePieceId: id }
+        const availableCells = calcAvailableCells(state.cellsById, id)
+
+        return { ...state, activePieceId: id, availableCells }
       }
 
       if (!state.activePieceId) {
@@ -116,7 +174,7 @@ const Grid: React.FC = () => {
       }
 
       if (state.activePieceId === id) {
-        return { ...state, activePieceId: null }
+        return { ...state, activePieceId: null, availableCells: [] }
       }
 
       const activeCell = state.cellsById[state.activePieceId]
@@ -126,12 +184,17 @@ const Grid: React.FC = () => {
         cellsById: {
           ...state.cellsById,
           [id]: { ...activeCell, id }, // put our active piece to the new cell
-          [activeCell.id]: { ...activeCell, player1: false, player2: false }, // clear the previous cell
+          [activeCell.id]: { ...activeCell, occupied: null }, // clear the previous cell
         },
         activePieceId: id,
       }
     })
   }, [])
+
+  const onGridClick = useCallback(
+    (event: React.MouseEvent<HTMLDivElement>) => {},
+    []
+  )
 
   return (
     <div className="grid-wrapper">
@@ -139,16 +202,27 @@ const Grid: React.FC = () => {
         <div
           className="grid"
           style={{ gridTemplateColumns: `repeat(${GRID_SIZE}, 1fr)` }}
+          onClick={onGridClick}
         >
-          {grid.map(({ id, isEmpty }) => (
-            <Cell
-              key={id}
-              onClick={onCellClick}
-              isEmpty={isEmpty}
-              isActive={id === state.activePieceId}
-              {...state.cellsById[id]}
-            />
-          ))}
+          {grid.map(({ id, isEmpty }) => {
+            const occupied = state.cellsById[id]?.occupied || null
+            const disabled = state.player1Turn
+              ? occupied !== 'player1'
+              : occupied !== 'player2'
+
+            return (
+              <Cell
+                key={id}
+                id={id}
+                onClick={onCellClick}
+                isEmpty={isEmpty}
+                isActive={id === state.activePieceId}
+                occupied={occupied}
+                disabled={disabled}
+                highlighted={state.availableCells.includes(id)}
+              />
+            )
+          })}
         </div>
       </div>
     </div>
