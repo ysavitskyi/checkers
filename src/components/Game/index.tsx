ac2d8f3162f72c6362b39c90f8459bcfb74fa31d
Yevhen Savitskyi <yevhen.savitskyi@gmail.com>
2023-05-01 10:54:15 +0300

    structured game component

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -9,15 +9,47 @@ type IBoardState = Exclude<
 >
 
 const Game = () => {
+  const historyAPI = useHistory()
+  const { history, historyIndex, addHistoryItem } = historyAPI
+
+  return (
+    <div className="game">
+      <div className="game-col game-col--left">
+        <Board
+          addHistoryItem={addHistoryItem}
+          historyItem={history[historyIndex]}
+        />
+      </div>
+      <div className="game-col game-col--right">
+        <Turn isWhite={historyIndex % 2 === 0} />
+        <History historyAPI={historyAPI} />
+      </div>
+    </div>
+  )
+}
+
+const Turn: React.FC<{ isWhite: boolean }> = ({ isWhite }) => {
+  return (
+    <div className="flex f-acenter">
+      <h2>Turn:</h2>
+      <div style={{ width: '4em', height: '4em', position: 'relative' }}>
+        <Piece isWhite={isWhite} disabled />
+      </div>
+    </div>
+  )
+}
+
+const History: React.FC<{ historyAPI: ReturnType<typeof useHistory> }> = ({
+  historyAPI,
+}) => {
   const {
     history,
     historyIndex,
-    addHistoryItem,
     setHistoryIndex,
     nextHistoryStep,
     prevHistoryStep,
     resetHistory,
-  } = useHistory()
+  } = historyAPI
 
   const jumpHistory = useCallback(
     (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -29,55 +61,41 @@ const Game = () => {
   )
 
   return (
-    <div className="game">
-      <div className="game-col game-col--left">
-        <Board
-          addHistoryItem={addHistoryItem}
-          historyItem={history[historyIndex]}
-        />
+    <>
+      <div className="flex f-acenter">
+        <h2>History:</h2>
+        &nbsp;
+        <button onClick={prevHistoryStep} disabled={historyIndex === 0}>
+          Prev
+        </button>
+        &nbsp;
+        <button
+          onClick={nextHistoryStep}
+          disabled={historyIndex === history.length - 1}
+        >
+          Next
+        </button>
+        &nbsp; ... &nbsp;
+        <button onClick={resetHistory} disabled={history.length <= 1}>
+          Reset
+        </button>
       </div>
-      <div className="game-col game-col--right">
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <h2>Turn:</h2>
-          <div style={{ width: '4em', height: '4em', position: 'relative' }}>
-            <Piece isWhite={historyIndex % 2 === 0} disabled />
-          </div>
-        </div>
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <h2>History:</h2>
-          &nbsp;
-          <button onClick={prevHistoryStep} disabled={historyIndex === 0}>
-            Prev
-          </button>
-          &nbsp;
-          <button
-            onClick={nextHistoryStep}
-            disabled={historyIndex === history.length - 1}
-          >
-            Next
-          </button>
-          &nbsp; ... &nbsp;
-          <button onClick={resetHistory} disabled={history.length <= 1}>
-            Reset
-          </button>
-        </div>
 
-        <ol>
-          {history.map((_, index) => (
-            <li key={index}>
-              <button
-                value={index}
-                onClick={jumpHistory}
-                type="button"
-                disabled={historyIndex === index}
-              >
-                Go to
-              </button>
-            </li>
-          ))}
-        </ol>
-      </div>
-    </div>
+      <ol>
+        {history.map((_, index) => (
+          <li key={index}>
+            <button
+              value={index}
+              onClick={jumpHistory}
+              type="button"
+              disabled={historyIndex === index}
+            >
+              Go to
+            </button>
+          </li>
+        ))}
+      </ol>
+    </>
   )
 }
 
